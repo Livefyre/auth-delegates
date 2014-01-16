0ace57b7f35f2cb926398ec33676cff541be3d60
Ross Pfahler <ross.pfahler@gmail.com>
2014-01-16 10:28:14 -0800

    Add initial version of remote auth delegate

---
+++ src/delegates/remote.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Remote auth delegate is the base for any custom implementations
+ * of auth for Livefyre.
+ */
+
+var storage = require('auth-delegates/util/storage'),
+    user = require('auth-delegates/user'),
+    AUTH_COOKIE_KEY = 'fyre-auth';
+
+/**
+ * @constructor
+ */
+function RemoteAuthDelegate() {}
+
+AuthDelegate.prototype.loadSession = function() {
+    var cookieData = storage.get(AUTH_COOKIE_KEY) || {};
+    if (cookieData['token']) {
+        user.loadSession(cookieData);
+    } else {
+        storage.remove(AUTH_COOKIE_KEY);
+    }
+};
+
+RemoteAuthDelegate.prototype.logout = function() {
+    user.logout();
+};
+
+// To be implemented by usef
+RemoteAuthDelegate.prototype.login = function() {};
+RemoteAuthDelegate.prototype.viewProfile = function() {};
+RemoteAuthDelegate.prototype.editProfile = function() {};
+
+module.exports = RemoteAuthDelegate;
